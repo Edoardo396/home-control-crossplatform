f51174669474c980f7ca148f5cc6e48dacba1f4f
Edoardo396 <edoardo.fullin@hotmail.it>
2017-06-05 22:30:12 +0200

    Some Request Handling

--- SharedLibrary/Network/MyRequestHandler.cpp
+++ SharedLibrary/Network/MyRequestHandler.cpp
@@ -1,5 +1,10 @@
 #include "../HomeControlController (Windows)/stdafx.h"
 #include "MyRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include <Poco/Format.h>
+#include "../Support/ConsoleLogger.h"
+#include <Poco/Net/HTTPServerResponse.h>
+#include <ostream>
 
 
 MyRequestHandler::MyRequestHandler() {
@@ -9,6 +14,18 @@ MyRequestHandler::MyRequestHandler() {
 MyRequestHandler::~MyRequestHandler() {
 }
 
-void MyRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) {
-	
+void MyRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) { 
+	response.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+	response.setContentType("text/html");
+	std::ostream& out = response.send();
+	out << "<h1>Hello world!</h1>" 
+		<< "<p>Host: " << request.getHost() << "</p>"
+		<< "<p>Method: " << request.getMethod() << "</p>"
+		<< "<p>URI: " << request.getURI() << "</p>";
+	out.flush();
+
+
+	std::cout << std::endl
+		<< " and URI=" << request.getURI() << std::endl;
+
 }
